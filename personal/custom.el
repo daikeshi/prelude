8936fc7cf15ad92a51ef0cd3b2c835c9089044af
Keshi Dai <keshi.dai@gmail.com>
2015-10-18 16:45:40 -0400

    Use my own color theme

--- personal/custom.el
+++ personal/custom.el
@@ -41,28 +41,12 @@
 
 (setq linum-format "%4d \u2502 ")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
- '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(fci-rule-color "#383838")
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; change the color of the current line highlight
-(set-face-background 'hl-line "#3e4446")
-(set-face-foreground 'highlight nil)
+(set-face-background 'default "#000000")
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#181b1c")
+(set-face-foreground 'highlight nil) 
 
 ;; start linum mode
 (global-linum-mode 1)
+(set-face-attribute 'linum nil :background "#000000")
